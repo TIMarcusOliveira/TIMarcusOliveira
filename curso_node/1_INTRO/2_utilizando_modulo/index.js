52c64e66842b49dd7d6d4352c47dcc551db74531
Marcus_Oliveira <151086480+TIMarcusOliveira@users.noreply.github.com>
2025-05-21 18:37:24 -0300

    Add files via upload

---
+++ curso_node/1_INTRO/2_utilizando_modulo/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs') //file system
+
+fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    console.log('File contents:', data);
+})
